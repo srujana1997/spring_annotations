be723f93811da8a5b0004185e64ed17e104860b8
srujana1997 <srujanadaruga@gmail.com>
2019-07-15 10:44:17 +0530

    task6

---
+++ src/main/java/com/stackroute/BeanPostProcessorDemoBean.java
@@ -0,0 +1,20 @@
+package com.stackroute;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization");
+        return bean;
+    }
+}
