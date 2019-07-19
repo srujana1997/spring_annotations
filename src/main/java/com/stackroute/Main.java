59febd9242ce2098fc5cbe5fa1d834f59320bb42
srujana1997 <51950151+srujana1997@users.noreply.github.com>
2019-07-19 10:59:10 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -6,12 +6,15 @@ import org.springframework.context.support.AbstractApplicationContext;
 
 public class Main {
     public static void main(String args[]){
+        //intializing applicationcontext object with classpath file
        AbstractApplicationContext applicationContext=new AnnotationConfigApplicationContext(configuration.class);
+        //getBean() returns the instance of a class
         Movie movieA=applicationContext.getBean(Movie.class);
         movieA.movieDisplay();
         Movie  movieB=applicationContext.getBean(Movie.class);
         movieB.movieDisplay();
         System.out.println(movieA == movieB);
+        //close the application context object
         applicationContext.close();
 
     }
