ffbf45f844455a087b03210fcdaf7ecf809284ae
srujana1997 <51950151+srujana1997@users.noreply.github.com>
2019-07-19 11:11:49 +0530

    Update Movie.java

--- src/main/java/com/stackroute/Movie.java
+++ src/main/java/com/stackroute/Movie.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//indication to create bean for this class
 public class Movie {
     @Autowired
+    //automatic dependency injection
     public void setActor(Actor actor) {
         this.actor = actor;
     }
